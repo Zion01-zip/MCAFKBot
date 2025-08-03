970f6fa62ac9b6ce42728de84f9290c16c9c08b1
Zion01-zip <zerone.ostria01@gmail.com>
2025-08-03 18:21:46 +0800

    Update bot-minecraft.js

--- bot-minecraft.js
+++ bot-minecraft.js
@@ -1,8 +1,9 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-    host: 'Lys_DeaDtest.aternos.me', // Replace with your server address
-    username: 'LysBotAFK', 
+    host: 'REIGNOFKINGS4.aternos.me', // Replace with your server address
+    port: '55997', // <<< ADD THIS LINE
+    username: 'STAFF', 
     version: '1.20.1', // Replace with your server version
     auth: 'offline', // Make your world cracked friendly
     checkTimeoutInterval: 300*1000 // Set a timeout to check if the bot is still connected (default is 5 minutes)
@@ -19,9 +20,5 @@ const welcome = async () => {
 
     welcome();
 }
-const advertise = () => {
-    bot.chat('AFK Bot By Lys_DeaD');
-}
 
-bot.on('spawn', advertise)
 bot.on('spawn', welcome)
